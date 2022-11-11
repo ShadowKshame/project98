1ac5c4cdd7364190a67ed30761036673236df51e
ShadowKshame <104213448+ShadowKshame@users.noreply.github.com>
2022-11-10 20:02:35 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -19,3 +19,51 @@ recognition.onresult = function(event)
     document.getElementById("textbox").innerHTML = Content;
 }
 
+function speak()
+{
+    var synth = window.speechSynthesis;
+
+    speak_data = "Taking your Selfie in 10 seconds";
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+    
+    synth.speak(utterThis); 
+    Webcam.attach(camera);  
+    setTimeout(function(){
+        take_snapshot();
+        save();
+    },5000);
+
+}
+
+function take_snapshot()
+{
+    console.log(img.id);
+
+    Webcam.snap(function(data_uri){
+        if(img_id=="selfie1"){
+            document.getElementById("camera1").innerHTML = '<img id="selfie1" src="'+data_uri+'"/>';
+        }
+        if(img_id=="selfie2"){
+            document.getElementById("camera2").innerHTML = '<img id="selfie2" src="'+data_uri+'"/>';
+        }
+        if(img_id=="selfie3"){
+            document.getElementById("camera3").innerHTML = '<img id="selfie3" src="'+data_uri+'"/>';
+        }
+    });
+}
+
+Webcam.set(
+{
+    width:360,
+    height:250,
+    image_format:"png",
+    png_quality:90
+});
+
+function save()
+{
+    link = document.getElementById("link");
+    image = document.getElementById("selfie_image").src;
+    link.href = image;
+    link.click();
+}
